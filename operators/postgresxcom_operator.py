d5fba7764ebf1d930ac965c760f807a39739e56f
J C Lawrence <claw@kanga.nu>
2017-03-08 17:08:01 -0800

    Forgot boilerplate

--- operators/postgresxcom_operator.py
+++ operators/postgresxcom_operator.py
@@ -3,6 +3,7 @@
 import logging, logtool, psycopg2.extras
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators import PostgresOperator
+from airflow.plugins_manager import AirflowPlugin
 
 LOG = logging.getLogger (__name__)
 
@@ -20,3 +21,7 @@ class PostgresXComOperator (PostgresOperator):
       for rec in cur:
         rc.append (rec)
     return rc
+
+class PostgresXComOperatorPlugin (AirflowPlugin):
+  name = "PostgresXComOperator"
+  operators = [PostgresXComOperator]
